7afcfe52db8b29a38284a5df9b432b7fca131792
HongEunho <ghdwns315@naver.com>
2021-02-02 00:39:16 +0900

    Update SoftwareStatusActivity.java

--- app/src/main/java/com/example/assistantapplication/SoftwareStatusActivity.java
+++ app/src/main/java/com/example/assistantapplication/SoftwareStatusActivity.java
@@ -1,13 +1,18 @@
 package com.example.assistantapplication;
 
+import androidx.annotation.IdRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
@@ -24,34 +29,88 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class SoftwareStatusActivity extends AppCompatActivity {
 
-    private EditText majorEdit;
-    private EditText statusEdit;
+    private TextView comment;
     private EditText commentEdit;
+    private EditText majorEdit;
+    private EditText positionEdit;
+    private EditText phoneNumEdit;
+    private RadioButton workBtn;
+    private RadioButton notworkBtn;
+    private RadioButton longnotBtn;
+    private RadioGroup workingGroup;
     private Button button;
     int staint;
+
+    public long now;
+    public Date mDate;
+    public SimpleDateFormat simpleDate;
+    public String formatDate;
+    String curName;
+    String myToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_software_status);
+        setContentView(R.layout.activity_computer_status);
         setTitle("소프트웨어학과 조교관리 시스템");
 
         final Activity a = SoftwareStatusActivity.this;
 
-        majorEdit = findViewById(R.id.majorEdit);
-        statusEdit = findViewById(R.id.statusEdit);
+        //현재 시간
+        now = System.currentTimeMillis() + 32400000;
+        mDate = new Date(now);
+        simpleDate = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        formatDate = simpleDate.format(mDate);
+        //현재 로그인 정보
+        SharedPreferences preferences = getSharedPreferences("login",MODE_PRIVATE);
+        String curID = preferences.getString("ID","0");
+        curName = preferences.getString("Name","0");
+        myToken = preferences.getString("Token",null);
+
+        comment = findViewById(R.id.comment);
         commentEdit = findViewById(R.id.commentEdit);
+        majorEdit = findViewById(R.id.majorEdit);
+        positionEdit = findViewById(R.id.positionEdit);
+        phoneNumEdit = findViewById(R.id.officeNumEdit);
+        commentEdit = findViewById(R.id.commentEdit);
+        workBtn = findViewById(R.id.workBtn);
+        notworkBtn = findViewById(R.id.notworkBtn);
+        longnotBtn = findViewById(R.id.longnotBtn);
+        workingGroup = findViewById(R.id.workingGroup);
         button = findViewById(R.id.button);
+
+        //기본적으로 comment는 숨기기
+        comment.setVisibility(View.GONE);
+        commentEdit.setVisibility(View.GONE);
+
         final String ser = ((ServerVariable)getApplicationContext()).getSer();
 
-        new JSONTask().execute(ser+"/status/소프트웨어학과");
+        new JSONTask().execute(ser+"/status/2");
+
+        RadioGroup.OnCheckedChangeListener radioGroupButtonChangeListener = new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                if(checkedId == R.id.workBtn)
+                {   staint = 0; comment.setVisibility(View.GONE); commentEdit.setVisibility(View.GONE);}
+                else if(checkedId == R.id.notworkBtn)
+                {   staint = 1; comment.setVisibility(View.GONE); commentEdit.setVisibility(View.GONE);}
+                else if(checkedId == R.id.longnotBtn)
+                {   staint = 2; comment.setVisibility(View.VISIBLE);  commentEdit.setVisibility(View.VISIBLE); }
+                else
+                {   staint = 0; comment.setVisibility(View.GONE); commentEdit.setVisibility(View.GONE);}
+            }
+        };
+        workingGroup.setOnCheckedChangeListener(radioGroupButtonChangeListener);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new JSONTask2().execute("http://mfam.site:3001status/소프트웨어학과");
+                new JSONTask2().execute(ser+"/status/2");
                 ((ServerVariable)getApplicationContext()).Cookie(a);
             }
         });
@@ -103,22 +162,34 @@ public class SoftwareStatusActivity extends AppCompatActivity {
             super.onPostExecute(result);
             //String id = jsonParsing(result);
             JSONObject jo = jsonParsing2(result);
+            String position="";
             String dep="";
             String sta="";
             String comment = "";
+            String phoneNum = "";
             try {
                 dep += jo.getString("department");
                 sta += jo.getString("status");
-
+                System.out.println("바뀐status:" + sta);
+                position += jo.getString("position");
+                phoneNum += jo.getString("phoneNumber");
                 comment += jo.getString("comment");
                 if(comment.equals("null"))
                     comment="없습니다";
             } catch (JSONException e) {
                 e.printStackTrace();
             }
+            //RadioButton으로 status나타내기
+            staint = Integer.parseInt(sta);
+            if(staint == 0) { workBtn.setChecked(true); notworkBtn.setChecked(false); longnotBtn.setChecked(false);      }
+            else if(staint == 1) { workBtn.setChecked(false); notworkBtn.setChecked(true); longnotBtn.setChecked(false);      }
+            else if(staint == 2) { workBtn.setChecked(false); notworkBtn.setChecked(false); longnotBtn.setChecked(true);      }
+            else { workBtn.setChecked(true); notworkBtn.setChecked(false); longnotBtn.setChecked(false);      }
+
             majorEdit.setText(dep);
-            statusEdit.setText(sta);
             commentEdit.setText(comment);
+            positionEdit.setText(position);
+            phoneNumEdit.setText(phoneNum);
         }
 
     }
@@ -127,10 +198,13 @@ public class SoftwareStatusActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(String... urls) {
             try {
-                staint = Integer.parseInt(statusEdit.getText().toString());
                 JSONObject jsonObject = new JSONObject();
                 jsonObject.accumulate("status", staint);
                 jsonObject.accumulate("comment", commentEdit.getText().toString());
+                jsonObject.accumulate("phoneNumber",phoneNumEdit.getText().toString());
+                jsonObject.accumulate("position",positionEdit.getText().toString());
+                jsonObject.accumulate("time",formatDate);
+                jsonObject.accumulate("modifier",curName);
 
                 HttpURLConnection con = null;
                 BufferedReader reader = null;
@@ -191,7 +265,6 @@ public class SoftwareStatusActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
-            statusEdit.setText(""+staint);
         }
     }
 
@@ -199,8 +272,7 @@ public class SoftwareStatusActivity extends AppCompatActivity {
     {
         JSONObject jo = null;
         try {
-            JSONArray ja = new JSONArray(json);
-            jo = ja.getJSONObject(0);
+            jo = new JSONObject(json);
         } catch (JSONException e) {
             e.printStackTrace();
         }
